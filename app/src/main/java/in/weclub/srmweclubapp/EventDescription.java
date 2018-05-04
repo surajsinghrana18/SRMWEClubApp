38ec469e96cff959bde20732fffead99cbb9aa46
triko88 <apan007@live.co.uk>
2018-05-04 17:15:07 +0530

    Add - Upcoming Event Support

--- app/src/main/java/in/weclub/srmweclubapp/EventDescription.java
+++ app/src/main/java/in/weclub/srmweclubapp/EventDescription.java
@@ -36,96 +36,97 @@ public class EventDescription extends AppCompatActivity {
         final FirebaseUser u = FirebaseAuth.getInstance().getCurrentUser();
         DatabaseReference ref = database.getReference("Event");
         DatabaseReference dr = database.getReference("Users");
-        dr.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                String g = dataSnapshot.child(u.getUid()).child("Registered Events").child(getIntent().
-                        getExtras().getString("Event ID"))
-                        .getValue(String.class);
-                if(g!=null)
-                    registered = g.equals("Registered");
-                else
-                    registered = false;
-            }
+            dr.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    String g = dataSnapshot.child(u.getUid()).child("Registered Events").child(getIntent().
+                            getExtras().getString("Event ID"))
+                            .getValue(String.class);
+                    if (g != null)
+                        registered = g.equals("Registered");
+                    else
+                        registered = false;
+                }
 
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
 
-            }
-        });
-        ref.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                String s = getIntent().getExtras().getString("Event ID");
-                DataSnapshot ds = dataSnapshot.child(s);
-                String name = ds.child("Event Name").getValue(String.class);
-                String spk = ds.child("Speaker").getValue(String.class);
-                String date = ds.child("Date").getValue(String.class);
-                String time = ds.child("Time").getValue(String.class);
-                String venue = ds.child("Venue").getValue(String.class);
-                String url = ds.child("Image").getValue(String.class);
-                String decp = ds.child("Description").getValue(String.class);
+                }
+            });
+            ref.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    String s = getIntent().getExtras().getString("Event ID");
+                    DataSnapshot ds = dataSnapshot.child(s);
+                    String name = ds.child("Event Name").getValue(String.class);
+                    String spk = ds.child("Speaker").getValue(String.class);
+                    String date = ds.child("Date").getValue(String.class);
+                    String time = ds.child("Time").getValue(String.class);
+                    String venue = ds.child("Venue").getValue(String.class);
+                    String url = ds.child("Image").getValue(String.class);
+                    String decp = ds.child("Description").getValue(String.class);
 
-                img = (ImageView)findViewById(R.id.imageView4);
-                Glide.with(EventDescription.this).load(url).into(img);
-                TextView n = findViewById(R.id.eventName1);
-                TextView det = findViewById(R.id.eventDet);
-                TextView des = findViewById(R.id.eventDesc);
-                des.setText(decp);
+                    img = (ImageView) findViewById(R.id.imageView4);
+                    Glide.with(EventDescription.this).load(url).into(img);
+                    TextView n = findViewById(R.id.eventName1);
+                    TextView det = findViewById(R.id.eventDet);
+                    TextView des = findViewById(R.id.eventDesc);
+                    des.setText(decp);
 
-                final String i = ds.getKey();
+                    final String i = ds.getKey();
 
-                n.setText(name);
-                det.setText(String.format("Speaker: %s\nDate: %s\nTime: %s\nVenue: %s",spk,date,time,venue));
+                    n.setText(name);
+                    det.setText(String.format("Speaker: %s\nDate: %s\nTime: %s\nVenue: %s", spk, date, time, venue));
 
-                b = (Button)findViewById(R.id.button2);
-                if(!registered){
-                //b.getBackground().clearColorFilter();
-                    b.setBackgroundColor(Color.rgb(228,10,9));
-                b.setOnClickListener(new View.OnClickListener() {
-                    final String f = i;
-                    @Override
-                    public void onClick(View view) {
-                        AlertDialog.Builder adb = new AlertDialog.Builder(EventDescription.this
-                        ,R.style.AlertDialogTheme);
-                        adb.setMessage(Html.fromHtml("<font color='#000000'>Do you want to register in this event?</font>"))
-                                .setPositiveButton("YES",
+                    b = (Button) findViewById(R.id.button2);
+                    if (!registered) {
+                        //b.getBackground().clearColorFilter();
+                        b.setBackgroundColor(Color.rgb(228, 10, 9));
+                        b.setOnClickListener(new View.OnClickListener() {
+                            final String f = i;
+
+                            @Override
+                            public void onClick(View view) {
+                                AlertDialog.Builder adb = new AlertDialog.Builder(EventDescription.this
+                                        , R.style.AlertDialogTheme);
+                                adb.setMessage(Html.fromHtml("<font color='#000000'>Do you want to register in this event?</font>"))
+                                        .setPositiveButton("YES",
+                                                new DialogInterface.OnClickListener() {
+                                                    @Override
+                                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                                        DatabaseReference ref1 = database.getReference("Users");
+                                                        ref1.child(u.getUid()).child("Registered Events").child(f).setValue("Registered");
+                                                        Toast.makeText(EventDescription.this, "Event Registration Successful", Toast.LENGTH_SHORT).show();
+                                                        registered = true;
+                                                        changeCol();
+                                                    }
+                                                }).setNegativeButton("NO",
                                         new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                                DatabaseReference ref1 = database.getReference("Users");
-                                ref1.child(u.getUid()).child("Registered Events").child(f).setValue("Registered");
-                                Toast.makeText(EventDescription.this, "Event Registration Successful", Toast.LENGTH_SHORT).show();
-                                registered = true;
-                                changeCol();
+                                            @Override
+                                            public void onClick(DialogInterface dialogInterface, int i) {
+                                                dialogInterface.cancel();
+                                            }
+                                        }).create();
+                                adb.setTitle(Html.fromHtml("<font color='#000000'>Confirm Registration</font>"));
+                                adb.show();
                             }
-                        }).setNegativeButton("NO",
-                                new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                                dialogInterface.cancel();
-                            }
-                        }).create();
-                        adb.setTitle(Html.fromHtml("<font color='#000000'>Confirm Registration</font>"));
-                        adb.show();
+                        });
+
+                        //b.getBackground().setColorFilter(Color.rgb(0,130,50), PorterDuff.Mode.MULTIPLY);
+                    } else {
+                        changeCol();
+                        //b.getBackground().setColorFilter(Color.rgb(0,130,50), PorterDuff.Mode.MULTIPLY);
                     }
-                });
-
-                //b.getBackground().setColorFilter(Color.rgb(0,130,50), PorterDuff.Mode.MULTIPLY);
                 }
-                else
-                {
-                    changeCol();
-                    //b.getBackground().setColorFilter(Color.rgb(0,130,50), PorterDuff.Mode.MULTIPLY);
-                }
-            }
-        @Override
-        public void onCancelled(DatabaseError databaseError) {
 
-        }
-    });
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+
+
     }
-
     private void changeCol()
     {
         b.setBackgroundColor(Color.rgb(0,100,13));
