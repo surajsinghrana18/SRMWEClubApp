885950ede7f63b4b91832e70740bbc785c92ff11
surajsinghrana18 <surajsinghrana10@gmail.com>
2018-09-22 18:41:04 +0530

    add UI Testing

---
+++ app/src/androidTest/java/in/weclub/srmweclubapp/LoginActivityTest.java
@@ -0,0 +1,47 @@
+package in.weclub.srmweclubapp;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import android.support.test.espresso.Espresso;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class LoginActivityTest{
+
+    private String mStringToBetyped;
+
+    @Rule
+    public ActivityTestRule<LoginActivity1> mActivityRule = new ActivityTestRule<>(LoginActivity1.class);
+
+    @Before
+    public void initValidString() {
+        // Specify a valid string.
+        mStringToBetyped = "Espresso";
+    }
+
+    @Test
+    public void changeText_sameActivity() {
+        // Type text and then press the button.
+        onView(withId(R.id.pass3))
+                .perform(typeText(mStringToBetyped), closeSoftKeyboard());
+        onView(withId(R.id.pass3)).perform(click());
+
+        // Check that the text was changed.
+        onView(withId(R.id.moNo))
+                .check(matches(withText(mStringToBetyped)));
+    }
+}
